0508ee2a67375150c35b57913154988b0bbf9a2f
hazee <hazeeshaik679@gmail.com>
2024-10-28 12:36:27 +0530

    fourth1 commit

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return "Hello,This is Srihitha"
+
 @app.route("/about")
 def about():
-    return "Welcomt to my about page"
+    return "Welcome to my about page"
 
 @app.route("/service")
 def service():
     return "Welcome to service page"
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
